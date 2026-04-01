65ffc8f8ce5fd2549ba60569f120f8bc38d4bc18
Anirudh Dhagate <89373895+anirudhdhagate@users.noreply.github.com>
2026-04-01 22:10:16 +0530

    Update SQL analysis.sql
    Results master table created.

--- Formula 1/SQL analysis.sql
+++ Formula 1/SQL analysis.sql
@@ -57,7 +57,54 @@ races_clean as (
 
   select raceId, year as race_year, round as race_round, circuitId, name as race_name, date(date) as race_date from races
 
+),
+
+circuits_clean as (
+
+  select circuitId, name as circuit_name, location as circuit_city, country as circuit_country, lat, lng, alt from circuits
+
+),
+
+
+status_clean as (
+
+select statusId, case when status.status = 'Finished' or status.status like '%Laps%' then 'is_finished' else 'is_dnf' end as race_status, case when status.status = 'Finished' or status.status like '%Laps%' then 'Finished' when status.status = 'Accident' or status.status = 'Collision' or status.status = 'Spun off' then 'Driver-related failure' else 'Mechanical / reliability failure' end as failure_type from status
+
+),
+
+cte_results_drivers as (
+
+  select * from results_clean a left join drivers_clean b on a.driverId = b.driverId
+
+),
+
+cte_results_constructors as (
+
+  select * from cte_results_drivers a left join constructors_clean b on a.constructorId = b.constructorId
+
+),
+
+cte_results_races as (
+
+  select * from cte_results_constructors a left join races_clean b on a.raceId = b.raceId
+
+),
+
+cte_results_circuits as (
+
+  select * from cte_results_races a left join circuits_clean b on a.circuitId = b.circuitId -- circuitId is present in races, not in results
+
+),
+
+
+-- 26753
+
+-- select * from status;
+
+results_master as (
+  
+  select * from cte_results_circuits a left join status_clean b on a.statusId = b.statusId
+
 )
 
-
-select * from circuits limit 5;
+select * from results_master limit 10;
